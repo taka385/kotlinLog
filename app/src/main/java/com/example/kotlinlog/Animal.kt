08dca0969e55a2fa6b939e563bb4243deafad1fd
taka385 <sonny.taka51@gmail.com>
2019-11-07 06:14:59 +0900

    Humanクラスの定義修正 再提出

--- app/src/main/java/com/example/kotlinlog/Animal.kt
+++ app/src/main/java/com/example/kotlinlog/Animal.kt
@@ -3,13 +3,11 @@ package com.example.kotlinlog
 abstract class Animal {
     var name: String
     var  age: Int
-    var hobby: String
 
 
-    constructor(name: String, age: Int, hobby: String) {
+    constructor(name: String, age: Int) {
         this.name = name
         this.age = age
-        this.hobby = hobby
     }
 
     abstract fun say()
--- app/src/main/java/com/example/kotlinlog/Human.kt
+++ app/src/main/java/com/example/kotlinlog/Human.kt
@@ -3,7 +3,12 @@ package com.example.kotlinlog
 import android.util.Log
 
 open class Human: Animal {
-    constructor(name: String, age: Int, hobby: String): super(name, age, hobby) {
+
+    var hobby: String
+
+
+    constructor(name: String, age: Int, hobby: String): super(name, age) {
+        this.hobby = hobby
     }
     override fun say() {
         Log.d("kotlintest", "「私の名前は" + this.name + "です。年は" + this.age + "歳" + "です。」")
